429700e20bcdb15186f84d132bb709047551fc81
Himanshu <73010003+gargHimanshu086745@users.noreply.github.com>
2024-09-08 11:17:23 +0530

    Add files via upload
    springBoot backend code

---
+++ 02-rest-api/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into todo (id, description, done, target_date,username)
+values (10001, 'Learn AWS', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date,username)
+values (10002, 'Get AWS Certified', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date,username)
+values (10003, 'Learn C++', false, CURRENT_DATE(), 'in28minutes');
+
+insert into todo (id, description, done, target_date,username)
+values (10004, 'Learn Spring Boot', false, CURRENT_DATE(), 'Himanshu');
+
+insert into todo (id, description, done, target_date,username)
+values (10005, 'Get AWS Certified', false, CURRENT_DATE(), 'Himanshu');
+
+insert into todo (id, description, done, target_date,username)
+values (10006, 'Learn C++', false, CURRENT_DATE(), 'Himanshu');
